acd8cd20be8867371c5aa8a0823169a5efb8efc4
Patrick Liu <zeldalink0515@gmail.com>
2017-02-07 12:20:27 +0800

    Implement basic nodejs scaffold.

---
+++ generators/app/index.js
@@ -0,0 +1,100 @@
+const path = require('path');
+
+const _glob = require('glob');
+const glob = require('thenify')(_glob);
+const Generator = require('yeoman-generator');
+
+const getBasename = folder => path.basename(folder);
+const isNotNodeModules = dir => dir !== 'node_modules';
+const validateName = name => !!name.trim().length;
+
+module.exports = class extends Generator {
+  prompting() {
+    return glob(`${this.destinationRoot()}/*/`)
+    .then(folders =>
+      this.prompt([{
+        type: 'input',
+        name: 'projectName',
+        message: 'Enter the project name:',
+        default: path.basename(this.destinationRoot()),
+        validate: validateName,
+      }, {
+        type: 'input',
+        name: 'ansibleDirName',
+        message: 'Enter the directory name to place ansible scripts:',
+        default: 'ansible',
+        validate: validateName,
+      }, {
+        type: 'confirm',
+        name: 'useSubprojects',
+        message: 'Is this project containing sub-projects?',
+        default: false
+      }, {
+        when: answers => answers.useSubprojects,
+        type: 'checkbox',
+        name: 'subProjects',
+        message: 'Select sub-projects to be deployed.',
+        choices: folders.map(getBasename).filter(isNotNodeModules),
+        validate: selected => !!selected.length
+      }, {
+        when: answers => answers.useSubprojects,
+        type: 'checkbox',
+        name: 'buildOnlySubProjects',
+        message: 'Any sub-project should be builded only? (Not behave as server, like front-end)',
+        choices: answers => answers.subProjects,
+        default: answers => (answers.subProjects.includes('client') ? ['client'] : []),
+      }]).then(answers => {
+        this.projectName = answers.projectName;
+        this.ansibleDirName = answers.ansibleDirName;
+
+        this.useSubprojects = answers.useSubprojects;
+        const subProjects = answers.subProjects;
+
+        this.buildOnlySubProjects = answers.buildOnlySubProjects;
+        this.serviceSubProjects = subProjects.filter(p => !this.buildOnlySubProjects.includes(p));
+      })
+    );
+  }
+
+  writing() {
+    this.fs.copy(
+      this.templatePath('ansible/**/*'),
+      this.destinationPath(this.ansibleDirName)
+    );
+    ['build', 'distribute', 'run'].forEach(step => {
+      this.fs.copyTpl(
+        this.templatePath(`${step}.yml.ejs`),
+        this.destinationPath(this.ansibleDirName, `playbooks/${step}.yml`),
+        this
+      );
+    });
+    this.fs.copyTpl(
+      this.templatePath('vars.yml.ejs'),
+      this.destinationPath(this.ansibleDirName, 'group_vars/all.yml'),
+      this
+    );
+    if (this.useSubprojects) {
+      this.serviceSubProjects.forEach(subProject => {
+        this.fs.copyTpl(
+          this.templatePath('Dockerfile.ejs'),
+          this.destinationPath(this.ansibleDirName, `files/${subProject}-Dockerfile.template`),
+          { subProject }
+        );
+        this.fs.copy(
+          this.templatePath('init.sh'),
+          this.destinationPath(this.ansibleDirName, `files/init-${subProject}.sh`)
+        );
+      });
+    } else {
+      this.fs.copyTpl(
+        this.templatePath('Dockerfile.ejs'),
+        this.destinationPath(this.ansibleDirName, 'files/Dockerfile.template'),
+        { subProject: null }
+      );
+      this.fs.copy(
+        this.templatePath('init.sh'),
+        this.destinationPath(this.ansibleDirName, `files/init.sh`)
+      );
+    }
+  }
+};
